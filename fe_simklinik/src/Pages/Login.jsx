9c5f61a5663e6509688e5398cac01bca9c17d345
Lyandiff <rinhanterm78@gmail.com>
2025-01-15 17:39:47 +0700

    fix(login): Change Logo Path

--- fe_simklinik/src/Pages/Login.jsx
+++ fe_simklinik/src/Pages/Login.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Card from "../Components/Card";
 import Input from "../Components/Input";
 import Submit from "../Components/Submit";
+import logo from "/logo1.png"
 import { useNavigate } from "react-router";
 
 const Login = function () {
   const navi = useNavigate();
   return (
     <>
-      <img src="/logo1.png" alt="Logo" />
+      <img src={logo} alt="Logo" />
       <Card title="Login">
         <Input label="Username" />
         <Input label="Password" type="password" />
